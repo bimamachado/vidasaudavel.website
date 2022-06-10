0b57b5db0783bc3ae13ad863e3c91f63e5f071d8
bimamachado <bimamachado@gmail.com>
2022-06-09 21:42:00 -0300

    Add files via upload

---
+++ assets/js/many.js
@@ -0,0 +1,34 @@
+
+document.addEventListener("contextmenu", function (e){
+    e.preventDefault();
+}, false);
+
+
+
+document.onkeypress = function (event) {
+event = (event || window.event);
+if (event.keyCode == 123) {return false;}
+if (event.keyCode == 93) {return false;}
+if (event.keyCode == 85) {return false;}
+if (event.keyCode == 80) {return false;}
+if (event.keyCode == 44) {return false;}
+if (event.keyCode == 73) {return false;}
+}
+document.onmousedown = function (event) {
+event = (event || window.event);
+if (event.keyCode == 123) {return false;}
+if (event.keyCode == 93) {return false;}
+if (event.keyCode == 85) {return false;}
+if (event.keyCode == 80) {return false;}
+if (event.keyCode == 44) {return false;}
+if (event.keyCode == 73) {return false;}
+}
+document.onkeydown = function (event) {
+event = (event || window.event);
+if (event.keyCode == 123) {return false;}
+if (event.keyCode == 93) {return false;}
+if (event.keyCode == 85) {return false;}
+if (event.keyCode == 80) {return false;}
+if (event.keyCode == 44) {return false;}
+if (event.keyCode == 73) {return false;}
+}
